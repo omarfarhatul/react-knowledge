55c0dc46a3534875a3375902ed4d43784a71614d
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-10-28 21:03:19 +0600

    update for Tailwind CSS installationn in React js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import React from 'react'
 
 function App() {
 
 
   return (
     <>
-      <h1 className=''>Knowledge Cafe</h1>
+      <h1 className='bg-red-300'>Knowledge Cafe</h1>
     </>
   )
 }
