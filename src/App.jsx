643e199957f414d2f35716d29610d90dca616f10
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-10-29 22:28:49 +0600

    Add five fack data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import React from 'react'
 import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
