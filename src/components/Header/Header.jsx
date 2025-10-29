82508febbf88e28d0fb1ab13fe80db08882d9a09
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-10-29 22:47:27 +0600

    add some folder

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import profile from '../../assets/images/profile.png'
 import React from 'react';
 
 const Header = () => {
     return (
-        <div>
+        <div className='flex'>
             <h1 className='text-6xl font-bold'>Knowledge Cafe</h1>
+            <img src={profile} alt="" />
         </div>
     );
 };
