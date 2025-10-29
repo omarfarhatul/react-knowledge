73860152ac9d5f7f0b6f13b9ae9e2405641a23dc
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-10-29 21:57:00 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import React from 'react'
+import Header from './components/Header/Header'
 
 function App() {
 
 
   return (
     <>
-      <h1 className='text-6xl bg-red-300'>Knowledge Cafe</h1>
+      <Header></Header>
     </>
   )
 }
