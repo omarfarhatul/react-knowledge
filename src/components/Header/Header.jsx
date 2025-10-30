dc4a3c1b1ea6aa5f4d5a17e5840cfd6998471b44
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-10-30 10:51:13 +0600

    Add blog data

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import profile from '../../assets/images/profile.png'
+import React from 'react';
 
 const Header = () => {
     return (
-        <div className='flex justify-between'>
-            <h1 className='text-6xl font-bold'>Knowledge Cafe</h1>
+        <header className='flex justify-between items-center p-4 mx-4 border-b-2'>
+            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
             <img src={profile} alt="" />
-        </div>
+        </header>
     );
 };
 
