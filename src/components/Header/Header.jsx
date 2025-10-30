52d882af7023587041220b1fef8afde32f21106a
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-10-30 10:30:36 +0600

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,8 @@
 import profile from '../../assets/images/profile.png'
-import React from 'react';
 
 const Header = () => {
     return (
-        <div className='flex'>
+        <div className='flex justify-between'>
             <h1 className='text-6xl font-bold'>Knowledge Cafe</h1>
             <img src={profile} alt="" />
         </div>
