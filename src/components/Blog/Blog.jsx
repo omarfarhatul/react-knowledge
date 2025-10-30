e682d98f0be8debc213dc992028c84d8c2488582
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-10-30 10:57:49 +0600

    Add props

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Blog = () => {
+const Blog = ({blog}) => {
+    console.log(blog);
     return (
         <div>
             
         </div>
     );
 };
+
+Blog.propTypes={
+    blog: PropTypes.object.isRequired
+}
 
 export default Blog;
