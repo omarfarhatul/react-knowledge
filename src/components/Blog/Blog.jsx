19e5bf6b006e79235b7e147df6525196aed36e11
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-10-31 21:17:40 +0600

    Added author_img, author, posted_date

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
-    const {title, author, author_img, cover}=blog;
+    const {title, cover, author, author_img, reading_time, posted_date}=blog;
     return (
         <div>
-            <img src={cover} alt={`Cover picture of the title ${title}`} />
+            <img className='w-1/2' src={cover} alt={`Cover picture of the title ${title}`} />
             <div className='flex justify-between'>
-                <div>
-
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
                 </div>
                 <div>
-                    
+                    <span>{reading_time} min read</span>
                 </div>
             </div>
             <h2 className="text-4xl">{title}</h2>
