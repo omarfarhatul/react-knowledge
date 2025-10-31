c00862cc8ba96a29ae0eef979b933004f8474fd3
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-10-31 20:37:54 +0600

    update

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
-    console.log(blog);
+    const {title}=blog;
     return (
         <div>
-            
+            <h2 className="text-4xl">{title}</h2>
         </div>
     );
 };
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -12,6 +12,9 @@ const Blogs = () => {
     return (
         <div className='md:w-2/3'>
             <h1 className='text-4xl'>Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blog=> <Blog key={blog.id} blog={blog}></Blog>)
+            }
         </div>
     );
 };
