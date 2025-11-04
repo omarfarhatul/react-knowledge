0c9752e043d5296621d7ba1d3c3367e458285cdd
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-11-04 20:19:31 +0600

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className='flex justify-between items-center p-4 max-w-7xl border-b-2 mx-auto'>
             <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
-            <img src={profile} alt="profile" />
+            <img src={profile} alt="profile_img" />
         </header>
     );
 };
