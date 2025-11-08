e6ceb6ff8671f39c49a5a2eac1569b1ddc665cc2
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-11-08 21:13:10 +0600

    finished

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -12,7 +12,7 @@ const Blogs = ({handleAddBookmark, handleMarkAsRead}) => {
 
     return (
         <div className='md:w-2/3'>
-            <h1 className='text-4xl'>Blogs: {blogs.length}</h1>
+            <h1 className='text-4xl mb-4'>Blogs: {blogs.length}</h1>
             {
                 blogs.map(blog => <Blog key={blog.id} blog={blog} handleAddBookmark={handleAddBookmark} handleMarkAsRead={handleMarkAsRead}></Blog>)
             }
