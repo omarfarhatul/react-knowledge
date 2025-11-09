78cecf7f3d640436672755eb5e36b311058c4c97
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-11-09 21:55:29 +0600

    space

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -16,6 +16,7 @@ const Blog = ({blog, handleAddBookmark, handleMarkAsRead}) => {
                         <p>{posted_date}</p>
                     </div>
                 </div>
+                
                 <div>
                     <span>{reading_time} min read</span>
                     <button onClick={()=>handleAddBookmark(blog)} className='ml-3 text-red-600 text-2xl'><FaBookmark></FaBookmark></button>
